58c62bc20fa578addf776f01600d9667159f3dd5
Michael Dlesk <michaeldlesk@gmail.com>
2022-07-25 22:12:34 -0500

    Delete Update.bat

--- Update.bat
+++
@@ -1,5 +0,0 @@
-@ECHO OFF
-TIMEOUT /t 1 /nobreak > NUL
-TASKKILL /IM "HiveProfitSwitcher.exe" > NUL
-MOVE "C:\Users\micha\source\repos\HiveOSProfitSwitcher\Update\*" "C:\Users\micha\source\repos\HiveOSProfitSwitcher"
-START "" /B "C:\Users\micha\source\repos\HiveOSProfitSwitcher\HiveProfitSwitcher\bin\Debug\HiveProfitSwitcher.exe"
